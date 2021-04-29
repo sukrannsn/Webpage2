c489e1c570970104f4e135b00708d9b837c06670
sukrannsn <sukrannsn@outlook.com>
2021-04-29 11:10:52 +0300

    Add project files.

---
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,54 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+
+
+        //    bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+        //            "~/vendor/bootstrap/css/bootstrap.min.css"));
+
+        //    bundles.Add(new ScriptBundle("~/bundles/all").Include(
+        //        "~/vendor/fontawesome-free/css/all.min.css"));
+
+        //    bundles.Add(new ScriptBundle("~/bundles/coming-soon").Include(
+        //      "~/css/coming-soon.min.css"));
+
+        //    bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+        //     "~/vendor/jquery/jquery.min.js"));
+        
+
+        //    bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+        //    "~/vendor/bootstrap/js/bootstrap.bundle.min.js"));
+
+        
+
+        //    bundles.Add(new ScriptBundle("~/bundles/coming-soon").Include(
+        //"~/js/coming-soon.min.js"));
+
+        }
+    }
+}
